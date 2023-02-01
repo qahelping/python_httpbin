f01bcd312f4ceab47a84364fea1c7deb3445fbf3
Helen Yanushevskaya <helen.yanushevskaya@exness.com>
2023-02-01 21:01:45 +0300

    - add first branch

--- test/test_first_example.py
+++ test/test_first_example.py
@@ -7,3 +7,9 @@ import pytest
 async def test_first_example():
     with allure.step("First example"):
         assert 1 + 2 == 3
+
+@pytest.mark.asyncio
+@allure.feature("First example")
+async def test_first_example():
+    with allure.step("First example"):
+        assert 1 + 2 == 3
