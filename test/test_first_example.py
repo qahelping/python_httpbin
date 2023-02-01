3968889af96ee39d98e198b50435e9104a8c2ece
Helen Yanushevskaya <helen.yanushevskaya@exness.com>
2023-02-01 22:06:38 +0300

    - 2

--- test/test_first_example.py
+++ test/test_first_example.py
@@ -55,3 +55,11 @@ async def test_first_example():
 async def test_first_example():
     with allure.step("First example"):
         assert 1 + 2 == 3
+
+@pytest.mark.asyncio
+@allure.feature("First example")
+async def test_first_example():
+    with allure.step("First example"):
+        assert 1 + 2 == 3
+
+        
